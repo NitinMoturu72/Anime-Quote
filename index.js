f5cfdbb8c08c35625e0030ac89ef4612e6407971
NitinMoturu72 <nitinmoturu72@gmail.com>
2025-05-20 19:09:19 -0700

    Basic UI added

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { quote: "", character: "", anime: "" });
+});
+
+app.get('/quote', async (req, res) => {
+    try {
+        const result = await axios.get('https://api.animechan.io/v1/quotes/random');
+        //console.log(result.data.data.content);
+        res.render('index.ejs', {
+            quote: result.data.content,
+            character: result.data.character,
+            anime: result.data.anime,
+
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching Quote');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
